461a55deed25cb9286b94f92d87ec6142ffbc5a5
Thiffany Gonçalves <thiffanygabrielle48@gmail.com>
2026-03-26 23:42:34 +0000

    feat: configurar tailwind e implementar hero inicial

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,15 @@
 export default function Hero() {
   return (
-    <div style={{
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "white"
-    }}>
-      <h1>TESTE CENTRALIZADO</h1>
-    </div>
+    <section className="h-screen flex flex-col items-center justify-center bg-white text-black px-6 text-center">
+      
+      <h1 className="text-5xl font-bold mb-4">
+        Divina Carne
+      </h1>
+
+      <p className="text-lg text-gray-600 max-w-xl">
+        Alimentando o mundo hoje. Preservando o amanhã.
+      </p>
+
+    </section>
   );
 }
